22ff84ee0da3d39d692b2c4957ddf8863d9a8df7
vishal gehlot <vishalgehlot334@gmail.com>
2024-06-13 16:35:22 +0530

    Added the get a single product route

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -10,3 +10,19 @@ exports.createProduct = async (req, res) => {
   const newData = await productModel.create(data);
   res.status(200).json({ newData });
 };
+
+exports.getAllProducts = async (req, res) => {
+  // this line o 16 has to talk to database and get the data -- thats a time taking prpcess
+  const allProducts = await productModel.find();
+  res.status(200).json(allProducts);
+};
+
+exports.getASpecificProduct = async (req, res) => {
+  //params -- body -- query
+  // if you send data in body that will be always a post request
+  //params your request type can be anything
+
+  const id = req.params.id;
+  const product = await productModel.findById(id);
+  res.status(200).json(product);
+};
--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -1,9 +1,16 @@
 const express = require("express");
-const { createProduct } = require("../controllers/productController");
+const { createProduct, getAllProducts, getASpecificProduct } = require("../controllers/productController");
 
 const router = express.Router();
 
+//
 router.post("/product", createProduct);
+//getting all the products
+router.get("/allproducts", getAllProducts);
+//give the id of your product after /getaproduct and then it will repose you i the product data
+router.get("/getaproduct/:id", getASpecificProduct);
+
+//update //delete
 
 
 
